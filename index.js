7178452d9ffa29d2d5ac59e55f4b527a3a67f328
JaredTrOr <trujillojaredalexander@gmail.com>
2023-01-05 16:59:28 -0600

    There was added the routes gile and added the .env file to the gitingore

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.urlencoded({extended: false}));
+app.use('view engine', 'ejs');
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
