3d9d6f51860c7bfb540ab0b3823f2bad13f7a505
JaredTrOr <trujillojaredalexander@gmail.com>
2023-01-05 17:44:19 -0600

    There was added the mysql connection

--- connection.js
+++ connection.js
@@ -1,6 +1,29 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
 const UserModel = require('./models/User');
 
-const connection = Sequelize(
-    
+const connection = new Sequelize(
+    process.env.DBNAME,
+    process.env.DBUSERNAME,
+    process.env.DBPASSWORD,
+    {
+        host: process.env.DBHOST,
+        dialect: 'mysql',
+        port: process.env.DBPORT
+    }
 )
+
+const User = UserModel(connection, Sequelize);
+
+connection.sync()
+.then(() => {
+    console.log('Connection!!');
+})
+.catch((err) => {
+    console.log(`ERROR: ${err}`);
+});
+
+module.exports = {
+    User
+}
